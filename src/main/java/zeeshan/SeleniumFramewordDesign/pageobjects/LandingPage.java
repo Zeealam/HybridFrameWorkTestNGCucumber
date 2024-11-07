ebf0ff134747ac8c08f466fff00428db0d698d8a
Zeealam <zeeshan.alam7@gmail.com>
2024-11-07 15:59:27 +0530

    Hybrid

---
+++ src/main/java/zeeshan/SeleniumFramewordDesign/pageobjects/LandingPage.java
@@ -0,0 +1,51 @@
+package zeeshan.SeleniumFramewordDesign.pageobjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import zeeshan.SeleniumFramewordDesign.abstractcomponents.AbstractComponent;
+
+public class LandingPage extends AbstractComponent{
+	
+	WebDriver driver;
+	
+	public LandingPage(WebDriver driver) {
+		super(driver);
+		this.driver= driver;
+		PageFactory.initElements(driver, this);
+	}
+	//How this @FindBy annotation know about driver Ans : there is a method called initElements 
+	//which have to write first which will take care of constructing using driver argument which we pass in method 
+	@FindBy(id="userEmail")
+	private WebElement userEmail;
+	
+	@FindBy(id="userPassword")
+	private WebElement password;
+	
+	@FindBy(id="login")
+	private WebElement submit;
+	
+	@FindBy(css="div[aria-label='Incorrect email or password.']")
+	private WebElement errorMessage;
+	
+	public ProductCatalogue loginApplication(String email , String pass) {
+		userEmail.sendKeys(email);
+		password.sendKeys(pass);
+		submit.click();
+		//ProductCatalogue productCatalogue= new ProductCatalogue(driver);
+		//return productCatalogue;
+		return new ProductCatalogue(driver);
+	}
+	
+	public void goTo() {
+		driver.get("https://rahulshettyacademy.com/client");
+	}
+	
+	public String getErrorMessage() {
+		waitForWebElementToAppear(errorMessage);
+		return errorMessage.getText();
+	}
+
+}
